c005ad2d88f49bebfbb16b7674a7393104fa6059
DemonicSavage <gabrielhuwe@gmail.com>
2022-07-06 13:41:25 -0300

    Added remove dupes feature

--- src/main.py
+++ src/main.py
@@ -4,18 +4,25 @@ import argparse
 import sys
 
 from downloader import CardDownloader, StillDownloader
-from organizer import CardOrganizer
+from organizer import CardOrganizer, StillOrganizer
 
 import json_utils
 import config
 
+
 def main():
-    argparser = argparse.ArgumentParser(prog="sifas_card_downloader", description="searches and downloads SIFAS cards")
-    argparser.add_argument('--all', action='store_true', help='update, download, and organize cards')
-    argparser.add_argument('--update', action='store_true', help="update the json file containing card data to reflect the current available cards in idol.st")
-    argparser.add_argument('--download', action='store_true', help="download the cards")
-    argparser.add_argument('--organize', action='store_true', help="organize downloaded cards in a directory per unit, then a subdirectory per idol")
-    argparser.add_argument('--stills', action='store_true', help='work with stills instead of cards (won\'t organize)')
+    argparser = argparse.ArgumentParser(
+        prog="sifas_card_downloader", description="searches and downloads SIFAS cards")
+    argparser.add_argument('--all', action='store_true',
+                           help='update, download, and organize cards')
+    argparser.add_argument('--update', action='store_true',
+                           help="update the json file containing card data to reflect the current available cards in idol.st")
+    argparser.add_argument(
+        '--download', action='store_true', help="download the cards")
+    argparser.add_argument('--organize', action='store_true',
+                           help="organize downloaded cards in a directory per unit, then a subdirectory per idol")
+    argparser.add_argument('--stills', action='store_true',
+                           help='work with stills instead of cards (won\'t organize)')
     # TODO: Do this:
     '''argparser.add_argument('--idol', action='store', help="search cards with provided idol name")
     argparser.add_argument('--rarity', action='store', help="search cards with provided card rarity")
@@ -26,16 +33,20 @@ def main():
     args = argparser.parse_args(args=None if sys.argv[1:] else ['--help'])
     card_searcher(config.CARDS_DIR, args)
 
+
 def card_searcher(path, args):
-    downloader = CardDownloader(path) if not args.stills else StillDownloader(path)
+    downloader = CardDownloader(
+        path) if not args.stills else StillDownloader(path)
     json_utils.load_cards(downloader.path, downloader.objs, still=args.stills)
     if args.update or args.all:
         downloader.update()
     if args.download or args.all:
         downloader.download_multi()
-    if args.organize or args.all and not args.stills:
-        card_organizer = CardOrganizer(path)
-        card_organizer.organize_cards()
+    if args.organize or args.all:
+        organizer = CardOrganizer(
+            path) if not args.stills else StillOrganizer(path)
+        organizer.organize()
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
--- src/organizer.py
+++ src/organizer.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import os
+
 import utils
 
 import consts
@@ -10,17 +12,32 @@ class CardOrganizer:
         self.data = []
         self.list = []
 
-    def split_filename(self, filename):
-        return filename.split("_")
-
-    def get_card_names(self):
+    def get_filenames(self):
         dir = [x for x in self.path.joinpath(
             consts.CARD_RESULTS_DIR).iterdir() if x.is_file() and x.suffix in [".jpg", ".png"]]
         return list(dir)
 
+    def remove_duplicates(self, paths):
+        prefixes = [str(prefix).split(".")[0] for prefix in paths]
+
+        for path in prefixes:
+            if prefixes.count(path) > 1:
+                jpg = Path(f"{path}.jpg")
+                try:
+                    os.remove(jpg)
+
+                    jpg = jpg.name
+                    split_jpg = self.split_filename(jpg)
+
+                    os.unlink(Path.joinpath(
+                        self.path, split_jpg[1], split_jpg[2], jpg))
+
+                except FileNotFoundError:
+                    pass
+
     def create_symlink(self, path):
         file_name = path.name
-        name = self.split_filename(file_name)
+        name = file_name.split("_")
         new_path = Path.joinpath(self.path, name[1], name[2])
         new_card = new_path.joinpath(file_name)
 
@@ -32,7 +49,32 @@ class CardOrganizer:
         except FileExistsError:
             pass
 
-    def organize_cards(self):
-        cards = self.get_card_names()
+    def organize(self):
+        cards = self.get_filenames()
+        self.remove_duplicates(cards)
+
+        cards = self.get_filenames()
         for card in cards:
             self.create_symlink(card)
+
+
+class StillOrganizer:
+    def __init__(self, path):
+        self.path = utils.init_path(path)
+
+    def get_filenames(self):
+        dir = [x for x in self.path.joinpath(
+            consts.STILL_RESULTS_DIR).iterdir() if x.is_file() and x.suffix in [".jpg", ".png"]]
+        return list(dir)
+
+    def organize(self):
+        paths = self.get_filenames()
+        prefixes = [str(prefix).split(".")[0] for prefix in paths]
+
+        for path in prefixes:
+            if prefixes.count(path) > 1:
+                jpg = Path(f"{path}.jpg")
+                try:
+                    os.remove(jpg)
+                except FileNotFoundError:
+                    pass
