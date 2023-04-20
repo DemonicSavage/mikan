d03cf596a69f42b20fe9199de69d7a2477c7616f
DemonicSavage <gabrielhuwe@gmail.com>
2023-04-19 22:49:37 -0300

    Added copyright notices, just to be sure.

--- sifas_card_downloader.bat
+++ sifas_card_downloader.bat
@@ -1,1 +1,15 @@
+REM Copyright (C) 2022-2023 DemonicSavage
+REM This file is part of SIFAS Card Downloader.
+
+REM SIFAS Card Downloader is free software: you can redistribute it and/or modify
+REM it under the terms of the GNU General Public License as published by
+REM the Free Software Foundation, version 3 of the License.
+
+REM SIFAS Card Downloader is distributed in the hope that it will be useful,
+REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+REM GNU General Public License for more details.
+
+REM You should have received a copy of the GNU General Public License
+
 @poetry run run %1
--- src/sifas_card_downloader/main.py
+++ src/sifas_card_downloader/main.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
+# Copyright (C) 2022-2023 DemonicSavage
+# This file is part of SIFAS Card Downloader.
+
+# SIFAS Card Downloader is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+
+# SIFAS Card Downloader is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+
 
 import asyncio
 import sys
